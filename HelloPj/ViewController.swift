10b538648b0efd64d6cf2bcf0dbdc0410ae19359
Igor Kozar <kozaryk99@gmail.com>
2021-05-19 01:55:44 +0300

    few things

--- HelloPj/ViewController.swift
+++ HelloPj/ViewController.swift
@@ -9,11 +9,22 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var view1: UIView!
+    @IBOutlet weak var view2: UIView!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        view1.layer.masksToBounds = true
+        view1.layer.cornerRadius = 10
+        view1.layer.borderWidth = 1
+        
+        view2.layer.masksToBounds = true
+        view2.layer.cornerRadius = 10
+        view2.layer.borderWidth = 1
+    
     }
-
 
 }
 
